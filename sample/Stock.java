373377931bc084ebcfcf1fa7a306f2c2ef586514
zerosum22 <zerosum3150@gmail.com>
2021-06-19 22:33:28 +0900

    もう１回コミット

--- sample/Stock.java
+++ sample/Stock.java
@@ -1,1 +1,34 @@
-// これから作成予定
+package sample;
+
+/** 在庫クラス */
+public class Stock {
+	/** 倉庫番号 */
+	private int storeNo;
+	/** 在庫数量 */
+	private int quantity;
+	
+	/** コンストラクタ */
+	public Stock(int storeNo, int quantity) {
+		super();
+		this.storeNo = storeNo;
+		this.quantity = quantity;
+	}
+
+	public int getStoreNo() {
+		return storeNo;
+	}
+
+	public void setStoreNo(int storeNo) {
+		this.storeNo = storeNo;
+	}
+
+	public int getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+	
+	
+}
